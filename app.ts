a3815100b169f0045f3953afa30d935e79d3914a
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-05-25 19:15:45 -0700

    Set-up project with node/express

---
+++ app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import users from './routes/users';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+});
+
+app.use("/api/v1/users", users);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
