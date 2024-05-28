39d2ef193c9a273bb19c9bb207ac8e8e917ad114
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-05-27 17:08:52 -0700

    Add routes for users when blocking

--- controller.ts
+++ controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import nodemailer from "nodemailer";
 import { pool, db, users, user_friends, chats, user_chats, messages, comments } from "./connect";
 import { eq, and } from "drizzle-orm";
 import dotenv from "dotenv";
