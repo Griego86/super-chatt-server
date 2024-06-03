abb921e8ba408011d2ed26ada4eb1f58d9ac0eb1
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-06-02 18:45:56 -0700

    Add chat route

---
+++ routes/chat.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { addFriendToChat, createChat, getChat, getChats, getUsersInChat, leaveChat, updateChat } from "../controller";
+
+const chats = express.Router();
+
+chats.route('/').post(createChat);
+chats.route('/user/:userId').get(getChats);
+chats.route('/:chatId').get(getChat).post(updateChat);
+chats.route('/participants/:chatId').get(getUsersInChat);
+chats.route('/chat/:chatId').post(leaveChat);
+chats.route('/chat/add/:chatId').post(addFriendToChat);
+
+export default chats;
